0492ec8288b8cb0985fc63b5d24716f5be32d915
Immanuel Washington <code.immanuelqrw@gmail.com>
2018-11-18 19:26:21 -0500

    Test structure
    Added basic test structure
    Currently non-functional

--- src/test/kotlin/com/immanuelqrw/gitbase/domain/RepositoryExtensionIT.kt
+++ src/test/kotlin/com/immanuelqrw/gitbase/domain/RepositoryExtensionIT.kt
@@ -1,15 +1,45 @@
 package com.immanuelqrw.gitbase.domain
 
 import io.mockk.junit5.MockKExtension
+import org.amshove.kluent.shouldEqual
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.kohsuke.github.GHRepository
+import org.kohsuke.github.GitHub
+import org.kohsuke.github.GitHubBuilder
 
 
 @ExtendWith(MockKExtension::class)
 class RepositoryExtensionIT {
+    private lateinit var gitHub: GitHub
+    private lateinit var repository: GHRepository
+
+    // TODO Add Property file
+    private val propertyFile = ""
+
+    // TODO Find example repository name
+    private val repositoryName = "test"
+
+    @BeforeAll
+    fun setUp() {
+        // TODO Delete all test repositories before running tests
+        gitHub = GitHubBuilder.fromPropertyFile(propertyFile).build()
+        repository = gitHub.getRepository(repositoryName)
+    }
+
+    // TODO Look up ways to initialize fake data
     @Nested
     inner class BaseMethods {
-        fun `create repository with valid input`() {}
+        @ParameterizedTest
+        @ValueSource(strings = ["test-1", "test-2", "test-3"])
+        fun `create repository with valid input`(repositoryName: String) {
+            val repository: GHRepository = gitHub.createRepository(repositoryName).create()
+
+            repository.name shouldEqual repositoryName
+        }
     }
 
     @Nested

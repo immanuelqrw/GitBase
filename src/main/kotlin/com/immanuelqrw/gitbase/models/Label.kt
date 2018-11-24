6ab6b82b7dcd79c455d196eb9a725c6cbb16ba19
Immanuel Washington <code.immanuelqrw@gmail.com>
2018-11-23 23:59:10 -0500

    Fixed Label configuration
    Add documentation for Label description
    Added description to standard labels in configuration

--- src/main/kotlin/com/immanuelqrw/gitbase/models/Label.kt
+++ src/main/kotlin/com/immanuelqrw/gitbase/models/Label.kt
@@ -1,13 +1,14 @@
 package com.immanuelqrw.gitbase.models
 
-// TODO Add description -- current package doesn't use but API does
 /**
  * Represents a Label called [name] with [color]
  *
  * @property name
  * @property color
+ * @property description
  */
 data class Label(
     val name: String,
-    val color: String?
+    val color: String?,
+    val description: String?
 )

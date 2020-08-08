30b1b33d5ca7c9fba1a006ae4cd30ebf6ff2585a
Immanuel Washington <code.immanuelqrw@gmail.com>
2020-08-08 13:00:41 -0400

    configuration[🔧]: Added description and aliases to labels
    - Added updated description to aliases
    - Included aliases for ease of transfer, but currently non-functional

--- src/main/kotlin/com/immanuelqrw/gitbase/models/Label.kt
+++ src/main/kotlin/com/immanuelqrw/gitbase/models/Label.kt
@@ -1,14 +1,16 @@
 package com.immanuelqrw.gitbase.models
 
 /**
- * Represents a Label called [name] with [color]
+ * Represents a Label called [name] with [color], described by [description] and replaces any other name in [aliases]
  *
  * @property name
  * @property color
  * @property description
+ * @property aliases
  */
 data class Label(
     val name: String,
     val color: String?,
-    val description: String?
+    val description: String?,
+    val aliases: List<String>?
 )
